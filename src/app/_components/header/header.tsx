cea33ba1713f3d14dcd217c02a23769220bfd9bd
f.ghasemi <hello.fatemeh1@gmail.com>
2024-12-30 10:49:11 +0330

    Fix header

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -4,7 +4,6 @@ const Header: React.FC = () => {
   const navList = [
     { name: "File" },
     { name: "Edit" },
-    { name: "Selection" },
     { name: "View" },
     { name: "Go" },
     { name: "Run" },
@@ -12,8 +11,8 @@ const Header: React.FC = () => {
     { name: "Help" },
   ];
   return (
-    <div className="px-4 flex">
-      <div>
+    <div className="px-4 flex justify-between border-b border-gray-900 shadow shadow-gray-900/50 items-center h-10">
+      <div className="flex gap-6">
         <Image
           src="/icons/VS-Code.svg"
           alt="logo"
@@ -21,7 +20,7 @@ const Header: React.FC = () => {
           height={15}
           className="w-4 h-4  md:w-5 md:h-5"
         />
-        <ul className="text-sm text-gray-300 flex">
+        <ul className="text-sm text-gray-300 md:flex hidden gap-2">
           {navList?.map((items, index) => (
             <li key={index} className="cursor-pointer">
               {items?.name}
@@ -30,40 +29,18 @@ const Header: React.FC = () => {
         </ul>
       </div>
       <div>
-        <div className="">
-          <h5 className="text-sm text-gray-300">
+        <div className="rounded-lg border border-gray-600 bg-gray-800 w-[200px] h-5 sm:h-6 sm:w-[350px] flex justify-center items-center">
+          <h5 className="sm:text-sm text-gray-300 text-[10px]">
             Fatemeh Qasemi - Visual Studio Code
           </h5>
         </div>
       </div>
-      <div>
-        <span className="w-2 h-2 bg-[#f1fa8c] rounded-full"></span>
-        <span className="w-2 h-2 bg-[#50fa7b] rounded-full"></span>
-        <span className="w-2 h-2 bg-red-700 rounded-full"></span>
+      <div className="flex gap-1">
+        <span className="w-2 h-2 md:w-4 md:h-4 bg-[#f1fa8c] rounded-full"></span>
+        <span className="w-2 h-2 md:w-4 md:h-4 bg-[#50fa7b] rounded-full"></span>
+        <span className="w-2 h-2 md:w-4 md:h-4 bg-[#f9826c] rounded-full"></span>
       </div>
     </div>
   );
 };
 export default Header;
-// <div className="h-10 w-full md:px-4 px-8 border-b border-gray-900 shadow shadow-gray-900/50 flex items-center justify-between">
-//   <div className="flex items-center">
-//     <Image src="/icons/VS-Code.svg" alt="logo" width={15} height={15} />
-//     <ul className="hidden 900px:flex md:gap-6 gap-3 md:text-sm text-[10px] text-gray-100">
-//       {navList?.map((items, index) => (
-//         <li key={index} className="cursor-pointer">
-//           {items?.name}
-//         </li>
-//       ))}
-//     </ul>
-//   </div>
-//   <div className="">
-//     <div className="w-[200px] md:w-[500px] h-6 rounded-lg border border-gray-600 bg-gray-800 flex justify-center items-center text-gray-100 md:text-sm text-[10px]">
-//       <h4>Fatemeh Qasemi - Visual Studio Code</h4>
-//     </div>
-//   </div>
-//   <div className="flex gap-2">
-//     <span className="md:w-4 md:h-4 w-2 h-2 bg-[#f1fa8c] rounded-full"></span>
-//     <span className="md:w-4 md:h-4 w-2 h-2 bg-[#50fa7b] rounded-full"></span>
-//     <span className="md:w-4 md:h-4 w-2 h-2 bg-red-700 rounded-full"></span>
-//   </div>
-// </div>
