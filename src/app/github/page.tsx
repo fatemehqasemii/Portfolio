2d385870a32962c1d31ec4029777147ebb180994
fatemehqasemii <fatemehqasemi750@gmail.com>
2025-01-10 18:31:01 +0330

    fix responsive github page

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -3,8 +3,8 @@ import CardGitHub from "../_components/card-github/card";
 
 const GitHub: React.FC = () => {
   return (
-    <div className=" p-0 sm:p-5 flex flex-col gap-6 overflow-y-auto h-full border border-blue-500">
-      <div className="bg-[#1f2428] border border-yellow-300">
+    <div className=" p-0 sm:p-5 flex flex-col gap-6 overflow-y-auto h-full ">
+      <div className="bg-[#1f2428] ">
         <div className="flex text-sm py-3 sm:py-0 sm:text-xl sm:flex-row flex-col font-bold text-gray-300 gap-5 items-center justify-center h-fit sm:h-[100px]">
           <p>fatemehqasemii</p>
           <p className="text-3xl text-blue-600">|</p>
@@ -13,7 +13,7 @@ const GitHub: React.FC = () => {
           <p>3 followers</p>
         </div>
       </div>
-      <div className="grid grid-cols-16 gap-6 border border-red-900">
+      <div className="grid grid-cols-16 gap-6 ">
         <CardGitHub />
       </div>
       <div className="flex flex-col gap-0 sm:gap-2 ">
